f9e06a76900ec094fabfcea047a49521ad11da10
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2024-09-27 09:14:13 -0300

    Configurações para deploy na heroku

--- src/test/java/br/com/unipe/UnipeApplicationTests.java
+++ src/test/java/br/com/unipe/UnipeApplicationTests.java
@@ -2,7 +2,9 @@ package br.com.unipe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class UnipeApplicationTests {
 
