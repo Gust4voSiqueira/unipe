7b625be8fac9ee25d0d5cf4f99d0ef86d71236d3
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2024-10-16 11:02:05 -0300

    Adiciona id na listagem de pets

--- src/main/java/br/com/unipe/entity/pets/response/ListPetsResponse.java
+++ src/main/java/br/com/unipe/entity/pets/response/ListPetsResponse.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class ListPetsResponse {
+    private Long id;
     private String pet;
     private String description;
     private String phone;
@@ -18,6 +19,7 @@ public class ListPetsResponse {
 
     public static ListPetsResponse fromPet(Pet pet, boolean isAddedByCurrentUser) {
         return ListPetsResponse.builder()
+                .id(pet.getId())
                 .pet(pet.getPet())
                 .description(pet.getDescription())
                 .phone(pet.getUser().getPhone())
